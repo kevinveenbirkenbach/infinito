0ead23e06336387c60b1cfe10074467c62e27cb7
Kevin Frantz <mail@kevin-frantz.de>
2018-09-06 20:33:54 +0200

    Optimized login messages

--- application/src/Controller/SecurityController.php
+++ application/src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  *
@@ -17,13 +18,16 @@ class SecurityController extends AbstractController
      *
      * @Route("/login", name="login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils,TranslatorInterface $translator): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         if ($error) {
             $this->addFlash('danger', $error->getMessage());
         }else{
-            $this->addFlash('success', "User loged in.");
+            $lastUsername = $authenticationUtils->getLastUsername();
+            if($lastUsername){
+                $this->addFlash('success', $translator->trans('User %user% loged in.',['user'=>$lastUsername]));
+            }
         }
         $this->addFlash('info', $authenticationUtils->getLastUsername());
         return $this->render("user/login.html.twig",[
