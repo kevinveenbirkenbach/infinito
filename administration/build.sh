989de394592e1c88aaea9b74464774536850a8fe
Kevin Frantz <mail@kevin-frantz.de>
2018-09-06 23:02:52 +0200

    Added schema update to build process

--- administration/build.sh
+++ administration/build.sh
@@ -1,2 +1,3 @@
 cp .env.dist docker-symfony/.env
 (cd $(dirname $(readlink -f ${0}))/../docker-symfony/ && docker-compose build && docker-compose up -d)
+bash $(dirname $(readlink -f ${0}))/schema-update.sh
