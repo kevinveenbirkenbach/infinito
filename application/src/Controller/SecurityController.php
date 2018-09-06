14af353747ff1cc831e8ec243910b21af7872a55
Kevin Frantz <mail@kevin-frantz.de>
2018-09-06 22:44:04 +0200

    Deleted unneccessary dependendys

--- application/src/Controller/SecurityController.php
+++ application/src/Controller/SecurityController.php
@@ -6,9 +6,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Translation\TranslatorInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\User;
 
 /**
  *
