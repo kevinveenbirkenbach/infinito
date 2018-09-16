6cf929ada7366311214c658a6fd2966e78352fe8
Kevin Frantz <mail@kevin-frantz.de>
2018-09-16 12:58:39 +0200

    Added FOS Rest Bundle

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
