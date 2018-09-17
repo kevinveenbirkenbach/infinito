a8781de51a35f3f7fea7ad9c47dc996b9ca0a364
Kevin Frantz <mail@kevin-frantz.de>
2018-09-17 13:15:36 +0200

    Implemented form templates

--- application/src/Form/NameSourceType.php
+++ application/src/Form/NameSourceType.php
@@ -2,11 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\NameSource;
 
 class NameSourceType extends AbstractType
 {
@@ -18,7 +18,7 @@ class NameSourceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => NameSource::class,
         ]);
     }
 }
