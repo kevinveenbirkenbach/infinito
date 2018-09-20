fb6cf53785782470c945e18cfe1a63ca63e9d26b
Kevin Frantz <mail@kevin-frantz.de>
2018-09-20 16:06:02 +0200

    Installed fixture bundle

--- application/config/bundles.php
+++ application/config/bundles.php
@@ -19,4 +19,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
