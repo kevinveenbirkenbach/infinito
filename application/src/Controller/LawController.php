07396f6c4aa2d90884566ae9d786ddaab18da8c8
Kevin Frantz <mail@kevin-frantz.de>
2018-10-04 20:23:57 +0200

    Optimized todos

--- application/src/Controller/LawController.php
+++ application/src/Controller/LawController.php
@@ -6,37 +6,53 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Law;
+
 /**
  *
  * @author kevinfrantz
- *        
+ * @todo Implement security!
  */
 class LawController extends AbstractEntityController
 {
+
     /**
+     *
      * @Route("/law/{id}.{_format}", defaults={"_format"="html"})
      */
-    public function show():Response{
-        //Implement
-    }
-    
+    public function show(): Response
+    {
     /**
+     *
+     * @todo Implement function!
+     */
+    }
+
+    /**
+     *
      * @Route("/law/{id}/right.{_format}", defaults={"_format"="html"})
      */
-    public function right(int $id):RedirectResponse{
-        //Implement
-    }
-    
+    public function right(int $id): RedirectResponse
+    {
     /**
+     *
+     * @todo Implement function!
+     */
+    }
+
+    /**
+     *
      * @Route("/law/{id}/node.{_format}", defaults={"_format"="html"})
      */
-    public function node(int $id):RedirectResponse{
-        //Implement
+    public function node(int $id): RedirectResponse
+    {
+    /**
+     *
+     * @todo Implement function!
+     */
     }
-    
+
     protected function setEntityName(): void
     {
         $this->entityName = Law::class;
     }
-
 }
