ebf97e43d98efd0ab30b9e344464089deeb2ebec
Kevin Frantz <mail@kevin-frantz.de>
2018-10-04 21:40:07 +0200

    Refactored menu events

--- application/src/Event/Menu/Subbar/SourceMenuEvent.php
+++ application/src/Event/Menu/Subbar/SourceMenuEvent.php
@@ -2,58 +2,9 @@
 
 namespace App\Event\Menu\Subbar;
 
-use Knp\Menu\FactoryInterface;
-use Knp\Menu\ItemInterface;
-use Symfony\Component\EventDispatcher\Event;
-use Symfony\Component\HttpFoundation\RequestStack;
+use App\Event\Menu\AbstractMenuEvent;
 
-class SourceMenuEvent extends Event
+class SourceMenuEvent extends AbstractMenuEvent
 {
     public const EVENT = 'app.menu.source.user';
-
-    /**
-     * @var FactoryInterface
-     */
-    private $factory;
-
-    /**
-     * @var ItemInterface
-     */
-    private $item;
-
-    /**
-     * @var RequestStack
-     */
-    private $request;
-
-    public function __construct(FactoryInterface $factory, ItemInterface $item, RequestStack $request)
-    {
-        $this->factory = $factory;
-        $this->item = $item;
-        $this->request = $request;
-    }
-
-    /**
-     * @return FactoryInterface
-     */
-    public function getFactory(): FactoryInterface
-    {
-        return $this->factory;
-    }
-
-    /**
-     * @return ItemInterface
-     */
-    public function getItem(): ItemInterface
-    {
-        return $this->item;
-    }
-
-    /**
-     * @return RequestStack
-     */
-    public function getRequest(): RequestStack
-    {
-        return $this->request;
-    }
 }
--- application/src/Event/Menu/Topbar/UserMenuEvent.php
+++ application/src/Event/Menu/Topbar/UserMenuEvent.php
@@ -2,58 +2,9 @@
 
 namespace App\Event\Menu\Topbar;
 
-use Knp\Menu\FactoryInterface;
-use Knp\Menu\ItemInterface;
-use Symfony\Component\EventDispatcher\Event;
-use Symfony\Component\HttpFoundation\RequestStack;
+use App\Event\Menu\AbstractMenuEvent;
 
-class UserMenuEvent extends Event
+class UserMenuEvent extends AbstractMenuEvent
 {
     public const EVENT = 'app.menu.topbar.user';
-
-    /**
-     * @var FactoryInterface
-     */
-    private $factory;
-
-    /**
-     * @var ItemInterface
-     */
-    private $item;
-
-    /**
-     * @var RequestStack
-     */
-    private $request;
-
-    public function __construct(FactoryInterface $factory, ItemInterface $item, RequestStack $request)
-    {
-        $this->factory = $factory;
-        $this->item = $item;
-        $this->request = $request;
-    }
-
-    /**
-     * @return FactoryInterface
-     */
-    public function getFactory(): FactoryInterface
-    {
-        return $this->factory;
-    }
-
-    /**
-     * @return ItemInterface
-     */
-    public function getItem(): ItemInterface
-    {
-        return $this->item;
-    }
-
-    /**
-     * @return RequestStack
-     */
-    public function getRequest(): RequestStack
-    {
-        return $this->request;
-    }
 }
