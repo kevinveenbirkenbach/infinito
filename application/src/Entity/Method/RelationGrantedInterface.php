5af5a6b5e2605f80fec39a6a19c625d8026a6a56
Kevin Frantz <mail@kevin-frantz.de>
2018-10-28 14:29:19 +0100

    Added hint

--- application/src/Entity/Method/RelationGrantedInterface.php
+++ application/src/Entity/Method/RelationGrantedInterface.php
@@ -9,5 +9,12 @@ use App\Entity\RelationInterface;
  */
 interface RelationGrantedInterface
 {
+    /**
+     * @deprecated Methods shouldn't be used on the entity level
+     * @param RelationInterface $relation
+     * @param string $layer
+     * @param string $right
+     * @return bool
+     */
     public function isGranted(RelationInterface $relation, string $layer, string $right): bool;
 }
