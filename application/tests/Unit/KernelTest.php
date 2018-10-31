cadf26b7aa2cac13204f71469f3c536f4b15cbe4
Kevin Frantz <mail@kevin-frantz.de>
2018-10-31 22:43:10 +0100

    Added test for symfony core

---
+++ application/tests/Unit/KernelTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Kernel;
+use App\Tests\AbstractTestCase;
+
+/**
+ * This tests just exist to keep the code covering high.
+ * They're also helpfull to see if the symfony core changed.
+ *
+ * @author kevinfrantz
+ */
+class KernelTest extends AbstractTestCase
+{
+    /**
+     * @var Kernel
+     */
+    protected $kernel;
+
+    public function setUp(): void
+    {
+        $this->kernel = new Kernel('test', false);
+    }
+
+    public function testLogDir(): void
+    {
+        $this->assertEquals(true, is_string($this->kernel->getLogDir()));
+    }
+
+    public function testConfigureContainer(): void
+    {
+        $this->expectException(\TypeError::class);
+        $this->assertNull($this->invokeMethod($this->kernel, 'configureContainer', [null, null]));
+    }
+
+    public function testConfigureRoutes(): void
+    {
+        $this->expectException(\TypeError::class);
+        $this->assertNull($this->invokeMethod($this->kernel, 'configureRoutes', [null, null]));
+    }
+}
