56e1fd5acd7f98a354ff0ef56e32b30527a087b2
Kevin Frantz <mail@kevin-frantz.de>
2018-10-31 08:49:47 +0100

    Added install to composer update

--- administration/composer-update.sh
+++ administration/composer-update.sh
@@ -1,1 +1,1 @@
-(cd $(dirname $(readlink -f ${0}))/../docker-symfony/ && docker-compose exec php composer update)
+(cd $(dirname $(readlink -f ${0}))/../docker-symfony/&& docker-compose exec php composer install && docker-compose exec php composer update)
