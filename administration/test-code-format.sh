7704739a0aec165802763f0b71a9d6d4e6681147
Kevin Frantz <mail@kevin-frantz.de>
2018-11-01 23:02:32 +0100

    reduced test code format on folder src out of performance reasons

--- administration/test-code-format.sh
+++ administration/test-code-format.sh
@@ -1,1 +1,1 @@
-(cd $(dirname $(readlink -f ${0}))/../docker-symfony/ && docker-compose exec php php ./vendor/bin/php-cs-fixer fix -v --diff --dry-run .)
+(cd $(dirname $(readlink -f ${0}))/../docker-symfony/ && docker-compose exec php php ./vendor/bin/php-cs-fixer fix -v --diff --dry-run ./src)
--- administration/test-everything.sh
+++ administration/test-everything.sh
@@ -1,4 +1,5 @@
 (cd $(dirname $(readlink -f ${0}))/  &&
+bash ./clear.sh &&
 bash ./schema-validate.sh &&
 bash ./test.sh &&
 bash ./test-code-format.sh 
