0c2a832136369e6efef898692ef8f4163ed34d02
Kevin Frantz <mail@kevin-frantz.de>
2018-11-04 15:11:16 +0100

    Added test for menuevent

---
+++ application/tests/Unit/Event/Menu/MenuEventTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Event;
+
+use PHPUnit\Framework\TestCase;
+use App\Event\Menu\MenuEvent;
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuEventTest extends TestCase
+{
+    /**
+     * @var MenuEvent
+     */
+    protected $menuEvent;
+
+    public function setUp(): void
+    {
+        $factory = $this->createMock(FactoryInterface::class);
+        $item = $this->createMock(ItemInterface::class);
+        $request = $this->createMock(RequestStack::class);
+        $this->menuEvent = new MenuEvent($factory, $item, $request);
+    }
+
+    public function testConstructor(): void
+    {
+        $this->assertInstanceOf(FactoryInterface::class, $this->menuEvent->getFactory());
+        $this->assertInstanceOf(ItemInterface::class, $this->menuEvent->getItem());
+        $this->assertInstanceOf(RequestStack::class, $this->menuEvent->getRequest());
+    }
+}
