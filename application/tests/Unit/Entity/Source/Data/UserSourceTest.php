00ae6412e62139d0b75873f096a4d4aec6570935
Kevin Frantz <mail@kevin-frantz.de>
2018-11-04 12:24:52 +0100

    Implemented some tests for UserSource and deleted unnecessary attributs

---
+++ application/tests/Unit/Entity/Source/Data/UserSourceTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace tests\unit\Entity\Source\Data;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\Source\Data\UserSourceInterface;
+use App\Entity\Source\Data\UserSource;
+use Doctrine\Common\Collections\Collection;
+use App\Entity\Source\Data\NameSourceInterface;
+
+class UserSourceTest extends TestCase
+{
+    /**
+     * 
+     * @var UserSourceInterface
+     */
+    public $userSource;
+    
+    public function setUp():void{
+        $this->userSource = new UserSource();
+    }
+    
+    public function testConstructor():void {
+        $this->assertInstanceOf(Collection::class, $this->userSource->getMemberships());
+        $this->assertInstanceOf(NameSourceInterface::class, $this->userSource->getNameSource());
+    }
+}
+
