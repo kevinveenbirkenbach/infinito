03ce3f5cdf4ad616a7c39a0d34bc958391956b7c
Kevin Frantz <mail@kevin-frantz.de>
2018-11-04 12:12:34 +0100

    Implemented test for name attribut

---
+++ application/tests/Unit/Entity/Attribut/NameAttributTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Entity\Attribut;
+
+use PHPUnit\Framework\TestCase;
+
+class NameAttributTest extends TestCase
+{
+    /**
+     * @var NameAttributInterface
+     */
+    public $name;
+    
+    public function setUp():void{
+        $this->name = new class implements NameAttributInterface{
+            use NameAttribut;
+        };
+    }
+    
+    public function testConstructor():void{
+        $this->expectException(\TypeError::class);
+        $this->name->getName();
+    }
+    
+    public function testAccessors():void{
+        $name = "hello world!";
+        $this->assertNull($this->name->setName($name));
+        $this->assertEquals($name, $this->name->getName());
+    }
+}
+
