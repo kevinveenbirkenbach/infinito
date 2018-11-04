d20b3f2504179e1f475ce7861fd1900ab29ccd81
Kevin Frantz <mail@kevin-frantz.de>
2018-11-04 13:14:11 +0100

    Implemented childs attribute test and code formating

--- application/tests/Unit/Entity/Meta/RelationTest.php
+++ application/tests/Unit/Entity/Meta/RelationTest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace tests\Unit\Entity\Meta;
 
 use PHPUnit\Framework\TestCase;
@@ -9,20 +10,20 @@ use Doctrine\Common\Collections\Collection;
 class RelationTest extends TestCase
 {
     /**
-     * 
      * @var RelationInterface
      */
     protected $relation;
-    
-    public function setUp():void{
+
+    public function setUp(): void
+    {
         $this->relation = new Relation();
     }
-    
-    public function testConstructor():void {
+
+    public function testConstructor(): void
+    {
         $this->assertInstanceOf(Collection::class, $this->relation->getChilds());
         $this->assertInstanceOf(Collection::class, $this->relation->getParents());
         $this->expectException(\TypeError::class);
         $this->relation->getSource();
     }
 }
-
