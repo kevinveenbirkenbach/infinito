1286717495d82b4a468628b42adecc763e385cb2
Kevin Frantz <mail@kevin-frantz.de>
2018-11-04 11:36:40 +0100

    Adapted namespace

--- application/src/DataFixtures/UsersFixtures.php
+++ application/src/DataFixtures/UsersFixtures.php
@@ -1,20 +1,20 @@
 <?php
-namespace DataFixtures;
+
+namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
 use App\Entity\User;
+
 class UsersFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        
     }
 
-    public function adminUser(){
+    public function adminUser()
+    {
         $admin = new User();
         $admin->getSource();
     }
-
 }
