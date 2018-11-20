7ca10cf1543754f6e2f59a5ad95c7115f18e9510
Kevin Frantz <mail@kevin-frantz.de>
2018-11-20 22:27:21 +0100

    Cleared also prod environment

--- administration/clear.sh
+++ administration/clear.sh
@@ -1,1 +1,1 @@
-(cd $(dirname $(readlink -f ${0}))/../docker-symfony/ && docker-compose exec php php /var/www/symfony/bin/console cache:clear)
+(cd $(dirname $(readlink -f ${0}))/../docker-symfony/ && docker-compose exec php php /var/www/symfony/bin/console cache:clear && docker-compose exec php php /var/www/symfony/bin/console cache:clear --env=prod)
