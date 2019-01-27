2ed83ab2846a5c39de9789b3372fc6b7d015e65f
Kevin Frantz <mail@kevin-frantz.de>
2019-01-27 16:24:57 +0100

    Rised CodeCoverage of AbstractAction to 100%

---
+++ application/symfony/tests/Unit/Domain/ActionManagement/Read/AbstractActionTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace tests\Unit\Domain\ActionManagement\Read;
+
+use PHPUnit\Framework\TestCase;
+use App\Domain\ActionManagement\ActionInterface;
+use App\Domain\ActionManagement\AbstractAction;
+use App\Domain\ActionManagement\ActionServiceInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use App\Exception\NotValidByFormException;
+
+/**
+ * @author kevinfrantz
+ */
+class AbstractActionTest extends TestCase
+{
+    /**
+     * @var ActionInterface
+     */
+    private $action;
+
+    /**
+     * @var ActionServiceInterface|MockObject
+     */
+    private $actionService;
+
+    public function setUp(): void
+    {
+        $this->actionService = $this->createMock(ActionServiceInterface::class);
+        $this->action = new class($this->actionService) extends AbstractAction {
+            public $isSecure;
+            public $validByForm;
+
+            protected function isSecure(): bool
+            {
+                return $this->isSecure;
+            }
+
+            protected function isValidByForm(): bool
+            {
+                return $this->validByForm;
+            }
+
+            protected function proccess()
+            {
+            }
+        };
+    }
+
+    public function testNotValidByFormException(): void
+    {
+        $this->action->isSecure = true;
+        $this->action->validByForm = false;
+        $this->expectException(NotValidByFormException::class);
+        $this->action->execute();
+    }
+}
