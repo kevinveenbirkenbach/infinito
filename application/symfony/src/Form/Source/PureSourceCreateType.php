d6999cb601c5f619d8159eb1519ac6f650330e9f
Kevin Frantz <mail@kevin-frantz.de>
2019-02-13 18:39:14 +0100

    Optimized PureSourceCreateType

--- application/symfony/src/Form/Source/PureSourceCreateType.php
+++ application/symfony/src/Form/Source/PureSourceCreateType.php
@@ -1,21 +1,42 @@
 <?php
-
 namespace App\Form\Source;
 
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Source\PureSource;
+use App\Attribut\SlugAttributInterface;
+use App\Attribut\ClassAttributInterface;
 
 /**
+ *
  * @author kevinfrantz
  */
 final class PureSourceCreateType extends SourceType
 {
+
     /**
+     *
      * {@inheritdoc}
      *
      * @see \Symfony\Component\Form\AbstractType::buildForm()
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('slug');
+        $builder
+        ->add(SlugAttributInterface::SLUG_ATTRIBUT_NAME)
+        ->add(ClassAttributInterface::CLASS_ATTRIBUT_NAME, SourceType::class, [
+            'mapped' => false
+        ]);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\Form\AbstractType::configureOptions()
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => PureSource::class
+        ]);
     }
 }
