0bdcdd761021eec6d5b45790b539e18144a14156
Kevin Frantz <mail@kevin-frantz.de>
2019-02-16 10:27:28 +0100

    Implemented ActionServiceIntegrationTest

---
+++ application/symfony/tests/Integration/Domain/ActionManagement/ActionServiceIntegrationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace tests\Integration\Domain\ActionManagement;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Domain\ActionManagement\ActionServiceInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * @author kevinfrantz
+ */
+class ActionServiceIntegrationTest extends KernelTestCase
+{
+    /**
+     * @var ActionServiceInterface
+     */
+    private $actionService;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \PHPUnit\Framework\TestCase::setUp()
+     */
+    public function setUp(): void
+    {
+        self::bootKernel();
+        $this->actionService = self::$container->get(ActionServiceInterface::class);
+    }
+
+    public function testEnityManager(): void
+    {
+        $this->assertInstanceOf(EntityManagerInterface::class, $this->actionService->getEntityManager());
+    }
+}
