c702ddf2119afa9942f1d98528c2cd5e2b00aca4
Kevin Frantz <mail@kevin-frantz.de>
2019-02-18 22:20:51 +0100

    Implemented ActionIconClassMapTest

---
+++ application/symfony/tests/Unit/Domain/TwigManagement/ActionIconClassMapTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace tests\Unit\Domain\TwigManagement;
+
+use PHPUnit\Framework\TestCase;
+use Infinito\Domain\TwigManagement\ActionIconClassMapInterface;
+use Infinito\Domain\TwigManagement\ActionIconClassMap;
+use Infinito\Exception\NotSetException;
+use Infinito\DBAL\Types\ActionType;
+
+/**
+ * @author kevinfrantz
+ */
+class ActionIconClassMapTest extends TestCase
+{
+    /**
+     * @var ActionIconClassMapInterface
+     */
+    private $actionIconClassMap;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \PHPUnit\Framework\TestCase::setUp()
+     */
+    public function setUp(): void
+    {
+        $this->actionIconClassMap = new ActionIconClassMap();
+    }
+
+    public function testException(): void
+    {
+        $this->expectException(NotSetException::class);
+        $this->actionIconClassMap->getIconClass('wejfhwhke12');
+    }
+
+    public function testAllActionsSet(): void
+    {
+        foreach (ActionType::getChoices() as $action) {
+            $this->assertIsString($this->actionIconClassMap->getIconClass($action));
+        }
+    }
+}
