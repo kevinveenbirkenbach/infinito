9a6ab093f2aa7baa60d70bbc9c703b97e8054a03
Kevin Veen-Birkenbach [aka. Frantz] <kevin@veen.world>
2020-04-02 22:14:50 +0200

    Removed wrong comment

--- application/symfony/src/Domain/Form/FormClassNameServiceInterface.php
+++ application/symfony/src/Domain/Form/FormClassNameServiceInterface.php
@@ -2,16 +2,12 @@
 
 namespace Infinito\Domain\Form;
 
-use Infinito\Entity\EntityInterface;
-
 /**
  * @author kevinfrantz
  */
 interface FormClassNameServiceInterface
 {
     /**
-     * @param EntityInterface $entity
-     *
      * @return string The name of the form of the entity
      */
     public function getClass(string $origineClass, string $type = ''): string;
